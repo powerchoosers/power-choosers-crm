1aaa8266e861f1934a560ace9083351410581523
powerchoosers <l.patterson@powerchoosers.com>
2025-08-07 10:46:36 -0500

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // This file contains the Firebase configuration and initialization.
 // It should be loaded before any other script that interacts with Firestore.
-[cite_start]// The purpose of this file is to provide universal configuration. [cite: 14]
+// The purpose of this file is to provide universal configuration. 
 
 const firebaseConfig = {
-    [cite_start]apiKey: "AIzaSyBKg28LJZgyI3J--I8mnQXOLGN5351tfaE", [cite: 24]
+    apiKey: "AIzaSyBKg28LJZgyI3J--I8mnQXOLGN5351tfaE",
     authDomain: "power-choosers-crm.firebaseapp.com",
-    [cite_start]projectId: "power-choosers-crm", [cite: 21]
-    storageBucket: "power-choosers-crm.firebasestorage.app",
-    [cite_start]messagingSenderId: "792458658491", [cite: 22]
+    projectId: "power-choosers-crm",
+    storageBucket: "power-choosers-crm.appspot.com",
+    messagingSenderId: "792458658491",
     appId: "1:792458658491:web:a197a4a8ce7a860cfa1f9e",
     measurementId: "G-XEC3BFHJHW"
 };
