15829a23d4a816760242c9802c1de4ec10d58ca9
powerchoosers <l.patterson@powerchoosers.com>
2025-09-11 21:46:58 -0500

    Add files via upload

--- api/calls.js
+++ api/calls.js
@@ -4,7 +4,7 @@ const { admin, db } = require('./_firebase');
 // CORS middleware
 function corsMiddleware(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     
     if (req.method === 'OPTIONS') {
